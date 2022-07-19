59e92b4e5a7b3e746890e2d0e20ddcbeb63c75cc
sayan95 <sharthakmallik@gmail.com>
2022-07-19 22:30:50 +0530

    Basket api enetities defined

---
+++ src/Gemma.Basket.API/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Gemma.Basket.API.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public BaseEntity()
+        {
+            CreatedOn = DateTime.UtcNow;
+            UpdatedOn = DateTime.UtcNow;
+        }
+
+    }
+}
